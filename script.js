d9af9e1b924a01244293f25af8ea149334154d28
Old Prego <spymasterluke@gmail.com>
2021-03-01 09:00:42 -0600

    Added To do's

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+function generatePassword(){
+
+}
+
+// TODO: Set up prompts
+// TODO: Set up character arrays
+// TODO: Randomly Choose from arrays based on how many characters are requested
+// TODO: Guarantee that at least one of each selected character type is included
+// TODO:  Maybe check if I can loop once through each array first and then at the end shuffle them.
+// TODO:  Use Fisher-Yates shuffle, which is apparently the standard. 
