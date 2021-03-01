d876bd703a86f40b04837a6d20209c35d38d7d8a
Old Prego <spymasterluke@gmail.com>
2021-03-01 09:44:26 -0600

    Set up initial statuses for different characters, including adding at least one of each. Next up is to start selecting from all of them based on the array selector number

--- script.js
+++ script.js
@@ -15,21 +15,18 @@ generateBtn.addEventListener("click", writePassword);
 
 //Generate Arrays
 function genArray(first,last){
-
   var numArray = [];
   var x = first;
-
   for (x; x <= last; x++){
     numArray.push(x);
   }
-
   return numArray;
 }
 
 var lowCharArr = genArray(97,122);
 var capCharArr = genArray(65,90);
 var numCharArr = genArray(48, 57);
-var speCharArr = genArray(33,47).concat(genArray(58,64).concat(genArray(91,96)));
+var speCharArr = genArray(33,47).concat(genArray(58,64)).concat(genArray(91,96)).concat(genArray(123,126));
 
 
 function generatePassword(){
@@ -38,9 +35,30 @@ function generatePassword(){
   var bNumberChar = window.confirm("Would you like numbers?");
   var bSpecialChar = window.confirm("Would you like special characters?");
 
+  var arraySelector = 1;
+  var passChar = [];
+  var code = lowCharArr[Math.floor(Math.random() * lowCharArr.length)];
+  passChar.push(String.fromCharCode(code));
+
+  if(bCapitalChar){
+    arraySelector++;
+    code = capCharArr[Math.floor(Math.random() * lowCharArr.length)];
+    passChar.push(String.fromCharCode(code));
+  }
+  if(bNumberChar){
+    arraySelector++;
+    code = numCharArr[Math.floor(Math.random() * lowCharArr.length)];
+    passChar.push(String.fromCharCode(code));
+  }
+  if(bSpecialChar){
+    arraySelector++;
+    code = speCharArr[Math.floor(Math.random() * lowCharArr.length)];
+    passChar.push(String.fromCharCode(code));
+  }
+
 }
 
-// TODO: Set up prompts
+//// TODO: Set up prompts
 // TODO: Set up character arrays
 // TODO:  Could I do this with math.random and randomizing over 4 different random statements instead?
 // TODO:  No, because certain arrays are separated and have a different base value than just 0.
